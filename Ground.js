c44b7340b70651659032403aa2e48116ed5a641e
Anish1818 <anish2408@outlook.com>
2020-11-19 18:43:11 +0530

    Changed Ground

--- Ground.js
+++ Ground.js
@@ -11,7 +11,7 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("brown");
+      fill(101,67,33);
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
